c9bff8d320a244c17cdf6d43a22ccf67ece80ab0
Marcin Jędrzejak <53284922+maartinj@users.noreply.github.com>
2023-02-27 10:28:26 +0100

    Completed Coding Workshop Part 5 - Bucket List Part 1 - The Basic List

--- BucketList/BucketListView.swift
+++ BucketList/BucketListView.swift
@@ -7,12 +7,42 @@
 
 import SwiftUI
 
+// Playlista: https://www.youtube.com/watch?v=C8FWpccVYro&list=PLBn01m5Vbs4CTP6FpTWG0ZPDTyj4Fpt-3&ab_channel=StewartLynch
+
 struct BucketListView: View {
-    let bucketList = ["Climb Everest", "Visit Hawaii", "Get Married"]
+    @State private var bucketList = ["Climb Everest", "Visit Hawaii", "Get Married"]
+    @State private var newItem = ""
     var body: some View {
-        List {
-            ForEach(bucketList, id: \.self) { item in
+        NavigationStack {
+            VStack {
+                HStack {
+                    TextField("New Bucket Item", text: $newItem)
+                        .textFieldStyle(.roundedBorder)
+                    Button {
+                        bucketList.append(newItem)
+                        newItem = ""
+                    } label: {
+                        Image(systemName: "plus.circle.fill")
+                    }
+                    .disabled(newItem.isEmpty)
+                }
+                .padding()
+                List {
+                    ForEach(bucketList, id: \.self) { item in
+                        NavigationLink(value: item) {
+                            Text(item)
+                        }
+                    }
+                    .onDelete { indexSet in
+                        bucketList.remove(atOffsets: indexSet)
+                    }
+                }
+                .listStyle(.plain)
+            }
+            .navigationTitle("Bucket List")
+            .navigationDestination(for: String.self) { item in
                 Text(item)
+                    .font(.title)
             }
         }
     }
